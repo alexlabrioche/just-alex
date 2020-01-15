d1888a4245edc0c4e9212d40cc6387aad2fb4ffd
Alex Baker <alexbakerdeveloper@gmail.com>
2020-01-15 20:13:50 +0100

    add styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
